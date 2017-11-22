d0650836ef35e0ddd1443d9800150825ceb56f21
Enrique Madrigal Gutierrez <emadriga@gmail.com>
2017-11-21 22:37:32 -0600

    Initial Commit

---
+++ mexar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mexar
+//
+//  Created by Enrique Madrigal Gutierrez on 21/11/17.
+//  Copyright © 2017 datalabor.com.mx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
